f6ae38111dc2305187f0dcb5b4651f2f8f713d55
Will Hughes <whukes@gmail.com>
2021-06-04 22:57:42 -0400

    fix worker encoder

--- worker/bin/app.js
+++ worker/bin/app.js
@@ -1,4 +1,4 @@
 const config = require('src/config');
 const { spawn } = require('child_process');
-var worker = spawn('nexrender-worker-win64.exe', ['--host=' + config.nexrender.host + ':' + config.nexrender.port, '--no-license', '--secret=' + config.nexrender.secret, '--multi-frames'],{env:{NEXRENDER_FFMPEG: 'c:\\ffmpeg.exe'}});
+var worker = spawn('nexrender-worker-win64.exe', ['--host=' + config.nexrender.host + ':' + config.nexrender.port, '--no-license', '--secret=' + config.nexrender.secret],{env:{NEXRENDER_FFMPEG: 'c:\\ffmpeg.exe'}});
 worker.stdout.on('data', data=>console.log(data.toString()));
