5eedd7283320ad42a8d61d89e6b16ede54fde731
Will Hughes <whukes@gmail.com>
2021-06-05 00:03:25 -0400

    worker fix

--- worker/bin/app.js
+++ worker/bin/app.js
@@ -1,4 +1,11 @@
 const config = require('src/config');
 const { spawn } = require('child_process');
-var worker = spawn('nexrender-worker-win64.exe', ['--host=' + config.nexrender.host + ':' + config.nexrender.port, '--no-license', '--secret=' + config.nexrender.secret],{env:{NEXRENDER_FFMPEG: 'c:\\ffmpeg.exe'}});
+var worker = spawn('nexrender-worker-win64.exe', [
+    '--host=' + config.nexrender.host + ':' + config.nexrender.port, 
+    '--no-license', 
+    '--secret=' + config.nexrender.secret, 
+    '--multi-frames',
+    '--workpath=' + config.nexreder.workpath
+    ]
+);
 worker.stdout.on('data', data=>console.log(data.toString()));
--- worker/src/config.js
+++ worker/src/config.js
@@ -5,5 +5,6 @@ config.nexrender = {};
 config.nexrender.host = process.env.NEXRENDER_HOST || 'http://localhost';
 config.nexrender.port = process.env.NEXRENDER_PORT || '9292';
 config.nexrender.secret = process.env.NEXRENDER_SECRET || 'ivcreator';
+config.nexrender.workpath = process.env.NEXRENDER_WORKPATH || 'c:\\ivcreator\\work';
 
 module.exports = config;
